0a06b56c43f5969d4b3e4fbcda68c73cf7eed4df
azikenp <azikenp26@gmail.com>
2024-11-24 16:37:44 +0100

    new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+import './App.css'
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+
+      <div className="app">
+        <Hero />
+        <Demo />
+      </div>
+    </main>
+  );
 }
