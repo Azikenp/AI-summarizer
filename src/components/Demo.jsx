cb0b5a326826d6bf6f9a40bdde338615e7b1ffb8
azikenp <azikenp26@gmail.com>
2024-11-29 22:36:01 +0100

    links done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
@@ -6,6 +6,16 @@ const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
   const [allArticles, setAllArticles] = useState([]);
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -17,6 +27,9 @@ const Demo = () => {
 
       setAllArticles(updatedAllarticles);
       setArticle(newArticle);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllarticles));
+
       console.log(newArticle);
     }
   };
@@ -49,7 +62,29 @@ const Demo = () => {
             ↵
           </button>
         </form>
+
+        {/* browse url history */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, i) => (
+            <div
+              key={`link-${i}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">{item.url}</p>
+            </div>
+          ))}
+        </div>
       </div>
+
+      
     </section>
   );
 };
