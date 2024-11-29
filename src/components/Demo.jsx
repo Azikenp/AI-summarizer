61f56cb8490c1ea2c3b72e3a9ade183539ad4956
azikenp <azikenp26@gmail.com>
2024-11-29 22:16:33 +0100

    data fetching

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,11 +1,24 @@
 import React, { useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
+  const [allArticles, setAllArticles] = useState([]);
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   const handleSubmit = async (e) => {
-    alert("submitted");
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      const updatedAllarticles = [newArticle, ...allArticles];
+
+      setAllArticles(updatedAllarticles);
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
   };
 
   return (
